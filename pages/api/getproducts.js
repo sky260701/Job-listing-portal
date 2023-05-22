75df0553fa1e5fe6ff478b9fa3b1304cdbd81eeb
Akashai4m <akash@ai4mtech.com>
2023-05-22 14:49:49 +0530

    FRDKF

--- pages/api/getproducts.js
+++ pages/api/getproducts.js
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introductionDGDDFBN
 
 import product from "../../models/product";
 import connectdb from "../../middleware/mongoose";
